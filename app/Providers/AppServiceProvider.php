540472101dbbf60838a99d775d1158aa4f5dbf1f
Kazumasa Yamasaki <yamasaki@elseif.jp>
2022-10-07 21:21:18 +0900

    SQLをログに出力する

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // デバッグ環境の場合はSQLログを出力する（/log/sql.log）
+        if (env('APP_DEBUG')) {
+            DB::listen(function ($query) {
+                $sql = $query->sql;
+                for ($i = 0; $i < count($query->bindings); $i++) {
+                    $sql = preg_replace("/\?/", "'" . $query->bindings[$i] . "'", $sql, 1);
+                }
+                // Log::channel('sql')->debug($sql);
+                $sqlLog = new Logger('SQL');
+                $sqlLog->pushHandler(new StreamHandler(storage_path('logs/sql.log')), Logger::DEBUG);
+                $sqlLog->debug($sql);
+            });
+        }
     }
 }
