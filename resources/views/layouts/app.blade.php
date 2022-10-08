40095c15d9d870b3c5b65d49f60fec56dcf974e3
Kazumasa Yamasaki <yamasaki@elseif.jp>
2022-10-08 20:55:35 +0900

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
                     @else
                     <div class="flex">
                         <div class="overflow-hidden relative w-6 h-6 bg-gray-100 rounded-full dark:bg-gray-600 mr-2">
-                            <svg class="absolute -left-1 w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 14 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+                            <svg class="absolute -left-1 w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 14 18" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
                         </div>
                         <span>{{ Auth::user()->name }}</span>
                     
@@ -87,15 +87,6 @@
                             <span class="flex-1 ml-3 whitespace-nowrap">ユーザー</span>
                             </a>
                         </li>
-                        <!-- <li>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
-                                {{ csrf_field() }}
-                            </form>
-                            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                            <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd"></path></svg>
-                            <span class="flex-1 ml-3 whitespace-nowrap">Logout</span>
-                            </a>
-                        </li> -->
                     </ul>
                 </div>
             </aside>
