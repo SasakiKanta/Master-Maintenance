1c8b7181b24c2e83a5cbb90a1f41e9facd2a01b8
Kazumasa Yamasaki <yamasaki@elseif.jp>
2022-10-28 12:04:24 +0900

    性別、都道府県Enum定義追加

---
+++ app/Enums/Gender.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Enums;
+
+/**
+ * 性別定義
+ */
+enum Gender: string
+{
+    case FEMALE = '1';
+    case MALE   = '2';
+    case OTHER  = '3';
+
+    /**
+     * 表示用のテキストを取得
+     */
+    public function label(): string
+    {
+        return match($this) {
+            self::FEMALE => '女性',
+            self::MALE   => '男性',
+            self::OTHER  => 'その他',       
+        };
+    }
+}
